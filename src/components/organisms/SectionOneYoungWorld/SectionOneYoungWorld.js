ea63abb0caea18f3287e0d73fa31b7e74de65066
ridham-dd <147288551+ridham-dd@users.noreply.github.com>
2024-07-16 08:55:14 -0500

    alt Image text added for the logo of the companies

--- src/components/organisms/SectionOneYoungWorld/SectionOneYoungWorld.js
+++ src/components/organisms/SectionOneYoungWorld/SectionOneYoungWorld.js
@@ -1,61 +1,77 @@
-import TitleText from "~/components/molecules/TitleText/TitleText"
-import Video from "../../atoms/videoAtom/Video"
+import TitleText from '~/components/molecules/TitleText/TitleText'
 
-export default function SectionTellYourStoryGetInvolved({
-    TextProps
-}) {
-    return (
-        <div className="text-center flex flex-col md:mb-[70px] md:mt-[70px]">
-            <div className="flex items-center flex-col max-w-[1200px] w-[100vw]">
-                <div className="flex md:w-1/2 px-7 flex-col items-center justify-center gap-11 mb-10">
-                    <TitleText
-                        //TitleText
-                        TitleSize="h1"
-                        TitleLabel={TextProps.title}
-                        TextSize="subtitle"
-                        TextLabel={TextProps.subtitle}
-                        ContainerTextProps="items-start"
-                        TextClassProps=""
-                        TitleClassProps=""
-                        ContainerTitleProps=""
-
-                    />
-                </div>
-                <div className="flex md:w-3/5 px-7 flex-col items-center justify-center gap-4 mb-5">
-                    <TitleText
-                        //TitleText
-                        TextSize="normal"
-                        TextLabel={TextProps.text}
-                        ContainerTextProps="text-start"
-                        TextClassProps=""
-                        TitleClassProps=""
-                        ContainerTitleProps=""
-                        TitleLabel=""
-                        TitleSize=""
-                    />
-                    <TitleText
-                        //TitleText
-                        TextSize="normal"
-                        TextLabel={TextProps.text2}
-                        ContainerTextProps="text-start"
-                        TextClassProps=""
-                        TitleClassProps=""
-                        ContainerTitleProps=""
-                        TitleLabel=""
-                        TitleSize=""
-                    />
-                </div>
-                <div className="flex md:hidden mt-[40px] align-center justify-center">
-                    <Video width="370px" height="208px" url="https://www.youtube.com/watch?v=hvJgwPNYnZo" />
-                </div>
-                <div className="hidden md:flex xl:hidden mt-[40px] align-center justify-center">
-                    <Video width="800px" height="450px" url="https://www.youtube.com/watch?v=hvJgwPNYnZo" />
-                </div>
-                <div className="hidden xl:flex mt-[40px] align-center justify-center">
-                    <Video width="1280px" height="720px" url="https://www.youtube.com/watch?v=hvJgwPNYnZo" />
-                </div>
-            </div>
-            
+export default function SectionTellYourStoryGetInvolved({ TextProps }) {
+  return (
+    <div className="flex flex-col text-center md:mb-[70px] md:mt-[70px]">
+      <div className="flex w-[100vw] max-w-[1200px] flex-col items-center">
+        <div className="mb-10 flex flex-col items-center justify-center gap-11 px-7 md:w-1/2">
+          <TitleText
+            //TitleText
+            TitleSize="h1"
+            TitleLabel={TextProps.title}
+            TextSize="subtitle"
+            TextLabel={TextProps.subtitle}
+            ContainerTextProps="items-start"
+            TextClassProps=""
+            TitleClassProps=""
+            ContainerTitleProps=""
+          />
         </div>
-    )
+        <div className="mb-5 flex flex-col items-center justify-center gap-4 px-7 md:w-3/5">
+          <TitleText
+            //TitleText
+            TextSize="normal"
+            TextLabel={TextProps.text}
+            ContainerTextProps="text-start"
+            TextClassProps=""
+            TitleClassProps=""
+            ContainerTitleProps=""
+            TitleLabel=""
+            TitleSize=""
+          />
+          <TitleText
+            //TitleText
+            TextSize="normal"
+            TextLabel={TextProps.text2}
+            ContainerTextProps="text-start"
+            TextClassProps=""
+            TitleClassProps=""
+            ContainerTitleProps=""
+            TitleLabel=""
+            TitleSize=""
+          />
+        </div>
+        <div className="align-center mt-[40px] flex justify-center md:hidden">
+          <iframe
+            width="370"
+            height="208"
+            src="https://www.youtube.com/embed/hvJgwPNYnZo?cc_load_policy=1"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="align-center mt-[40px] hidden justify-center md:flex xl:hidden">
+          <iframe
+            width="800"
+            height="450"
+            src="https://www.youtube.com/embed/hvJgwPNYnZo?cc_load_policy=1"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="align-center mt-[40px] hidden justify-center xl:flex">
+          <iframe
+            width="1280"
+            height="720"
+            src="https://www.youtube.com/embed/hvJgwPNYnZo?cc_load_policy=1"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  )
 }
