dea8e59ab29f1e224278d351b1ff1017a8dbc3f4
rupak07 <rpotdukhe@hawk.iit.edu>
2024-08-04 16:40:14 -0500

    Updated content of AI page

--- src/i18n/en/Ai.ts
+++ src/i18n/en/Ai.ts
@@ -3,28 +3,28 @@ export const Ai = {
     title: [
       {
         type: 'text',
-        text: 'Science behind ',
+        text: 'The science behind ',
       },
       {
         type: 'span',
         text: 'Virufy',
       },
     ],
-    text: 'Artificial Intelligence (AI) is capable of detecting minute differences in vocal pattern to detect disease presence.',
+    text: 'Artificial Intelligence (AI) is capable of detecting tiny differences in vocal patterns to detect disease presence.',
     url: '/publications',
-    linkText: 'See our Research',
+    linkText: 'Our Research',
   },
   aiSection: {
     title: 'How does AI Work?',
     aiCards: [
       {
         title: '01',
-        text: 'Our revolutionary CoughCheck app employs cutting-edge AI technology. By Simply coughing into your phone, our app captures and analyzes distinct cough patterns using sophisticated machine learning algorithms and neural networks.',
+        text: 'Our groundbreaking app employs cutting-edge AI technology to analyze your breathing patterns using deep learning algorithms and audio signal processing techniques.',
         style: 'tracking-widest text-[2.5rem] sm:text-6xl',
       },
       {
         title: '02',
-        text: 'It scrutinizes various elements of the cough, such as duration, frequency, and intensity. Leveraging a comprehensive database, the Al meticulously compares recorded cough patterns against known indicators of respiratory diseases, allowing for the early detection of potential health issues.',
+        text: 'It scrutinizes various elements of the cough, such as duration, frequency, and pitch. Leveraging a comprehensive database, the Al meticulously compares recorded cough patterns against known indicators of respiratory diseases, allowing for the early detection of potential health issues.',
         style: 'text-[2.5rem] sm:text-6xl',
       },
     ],
